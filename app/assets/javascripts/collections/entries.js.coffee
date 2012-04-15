faecbab05f9312dd6e163ba172c85f57bee6c02a
Andrew Hautau <arhautau@gmail.com>
2012-04-15 13:58:36 -0700

    Episode 1

---
+++ app/assets/javascripts/collections/entries.js.coffee
@@ -0,0 +1,4 @@
+class Raffler.Collections.Entries extends Backbone.Collection
+  url: '/api/entries'
+
+  model: Raffler.Models.Entry
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raffler::Application.config.secret_token = 'e4eb277a58f73a32e8c0aff927f8a1ec51aff0b82b5a09f7b977b33d15384c8461af5c87bc9332baf86233fcf6cdddb5b2714e32bec2a3f3e422fc6441906024'
